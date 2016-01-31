998a3a2e1779c3cbbc727fcd6fba65688c30c449
Shukri Shukriev <dark00v3@gmail.com>
2016-01-31 19:05:10 +0200

    Initial Commit

---
+++ saa_shukri_143610/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  saa_shukri_143610
+//
+//  Created by Shukri Shukriev on 1/31/16.
+//  Copyright © 2016 Shukri Shukriev. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
